41b1e4c9b3fdd726cd66661c8f25347502c33e92
vaishnavme <42497931+vaishnavme@users.noreply.github.com>
2021-04-18 15:29:34 +0530

    Context api created and exported

---
+++ src/context/data-context.js
@@ -0,0 +1,36 @@
+import { useContext ,createContext, useReducer } from "react";
+import {dataReducer} from "../reducer/data-reducer";
+
+const initailState = {
+    allVideos: [],
+    playList : [
+        {
+            id: "likedVideo",
+            name: "Liked Videos",
+            videos: []
+        },
+        {
+            id: "watchLater",
+            name: "Watch Later",
+            videos: []
+        }
+    ]
+}
+
+const DataContext = createContext();
+
+export function DataProvider({children}) {
+    const [state, dispatch] = useReducer(dataReducer, initailState);
+
+    return (
+        <DataContext.Provider value={{
+            allVideos: state.allVideos,
+            playList: state.playList,
+            dispatch
+        }}>
+            {children}
+        </DataContext.Provider>
+    )
+}
+
+export const useData = () => useContext(DataContext)
---
+++ src/context/index.js
@@ -0,0 +1,4 @@
+export { DataProvider } from "./data-context";
+export { useData } from "./data-context";
+export { AuthProvider } from "./auth-context";
+export { useAuth } from "./auth-context";
