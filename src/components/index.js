007c801961be4360f2c65beac1cc185602d73353
vaishnavme <42497931+vaishnavme@users.noreply.github.com>
2021-05-21 11:08:32 +0530

    loader com export

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { PlaylistCard } from "./PlaylistCard/PlaylistCard";
 export { VideoCard } from "./VideoCard/VideoCard";
 export { PrivateRoute } from "./PrivateRoute/PrivateRoute";
 export { LoginModal } from "./Modal/LoginModal";
+export { Loader } from "./Loader/Loader";
