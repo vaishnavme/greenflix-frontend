7e8eb3f9b2b77cb15b2475038469f4a6963b39db
vaishnavme <42497931+vaishnavme@users.noreply.github.com>
2021-05-23 10:09:11 +0530

    account page update

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,44 +1,31 @@
-import styles from "./Account.module.css";
 import { useAuth } from "../../context";
+import styles from "./Account.module.css";
 
-export default function Account() {
-    const { user } = useAuth(); 
+export default function Login() {
+    const { user, logOutUser } = useAuth();
+    
     return (
-        <div className={`${styles.card}`}>
-            <div className={`${styles.header}`}>
-                <h1>Hi {user.name}</h1>
-                <p className={`${styles.subtitle}`}>Update Account Settings</p>
+        <div className={`${styles.main}`}>
+            <div className={`${styles.card} p-2`}>
+                <div className={`${styles.header}`}>
+                    <h6>Hi there👋</h6>
+                    <h4><span className={`f-warning`}>{user.name}</span></h4>                 
+                </div>
+                <div className={`${styles.body}`}>
+                    <form>
+                        <div className={`styled-input`}>
+                            <input
+                                value={user.email}
+                                type="email" 
+                                readOnly 
+                                placeholder="Enter your email" 
+                                required/>
+                            <span></span>
+                        </div>
+                    </form>
+                    <button className={`btn btn-secondary`} onClick={() => logOutUser()}>Log Out</button>
+                </div>
             </div>
-            <div>
-                <form>
-                <div className={`styled-input`}>
-                        <input 
-                            type="email" 
-                            placeholder="Enter your name"
-                            required/>
-                        <span></span>
-                    </div>
-                    <div className={`styled-input`}>
-                        <input 
-                            type="email" 
-                            placeholder="Enter your email"
-                            required/>
-                        <span></span>
-                    </div>
-                    <div className={`styled-input`}>
-                        <input 
-                            type="password" 
-                            placeholder="Enter your password"
-                            required/>
-                        <span></span>
-                    </div>
-                </form>
-
-            </div>
-            <button 
-                className={`btn btn-primary lg`}>
-                    Sign In
-            </button>
         </div>
     )
 }
