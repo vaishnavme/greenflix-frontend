96a458607c7f7a62b5e4b3907817b7e3f4476504
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-09-07 14:28:57 +0530

    Style : update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -89,10 +89,24 @@
 
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useAuth, useData } from '../../context';
 import styles from './Navbar.module.css';
 
 export const Navbar = () => {
     const [isNavActive, setNavActive] = useState(false);
+    const { user, logOutUser } = useAuth();
+    const { dispatch } = useData();
+
+    const logout = () => {
+        logOutUser();
+        dispatch({ type: 'LOGOUT_USER_STATES' });
+    };
+
+    const activeStyle = {
+        color: '#1F2937',
+        backgroundColor: '#FDE68A'
+    };
+
     return (
         <>
             <header className={styles.header}>
@@ -112,24 +126,54 @@ export const Navbar = () => {
                 }
             >
                 <ul className={styles.navItem}>
-                    <NavLink className={styles.navLink} to="/">
+                    <NavLink
+                        activeStyle={activeStyle}
+                        className={styles.navLink}
+                        to="/"
+                        end
+                    >
                         <i className="bx bx-home"></i>
                         <span className={styles.linkText}>Home</span>
                     </NavLink>
-                    <NavLink className={styles.navLink} to="/playlist">
+                    <NavLink
+                        activeStyle={activeStyle}
+                        className={styles.navLink}
+                        to="/playlist"
+                    >
                         <i className="bx bxs-playlist"></i>
                         <span className={styles.linkText}>Playlist</span>
                     </NavLink>
-                    <NavLink className={styles.navLink} to="/account">
-                        <i className="bx bx-user"></i>{' '}
-                        <span className={styles.linkText}>Account</span>
-                    </NavLink>
+                    {user && (
+                        <NavLink
+                            activeStyle={activeStyle}
+                            className={styles.navLink}
+                            to="/account"
+                        >
+                            <i className="bx bx-user"></i>{' '}
+                            <span className={styles.linkText}>Account</span>
+                        </NavLink>
+                    )}
+                    {!user && (
+                        <NavLink
+                            activeStyle={activeStyle}
+                            className={styles.navLink}
+                            to="/login"
+                        >
+                            <i className="bx bx-user"></i>{' '}
+                            <span className={styles.linkText}>Log In</span>
+                        </NavLink>
+                    )}
                 </ul>
 
-                <button className={styles.btnLogout}>
-                    <i className="bx bx-log-out"></i>
-                    <span className={styles.linkText}>Log out</span>
-                </button>
+                {user && (
+                    <button
+                        onClick={() => logout()}
+                        className={styles.btnLogout}
+                    >
+                        <i className="bx bx-log-out"></i>
+                        <span className={styles.linkText}>Log out</span>
+                    </button>
+                )}
             </nav>
         </>
     );
