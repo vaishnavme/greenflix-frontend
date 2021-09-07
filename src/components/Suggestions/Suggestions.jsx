e2438e87c6e3fdd4e3cf7bff359d9a408b8b6a73
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-09-07 13:46:34 +0530

    basic: suggestion component created

---
+++ src/components/Suggestions/Suggestions.jsx
@@ -0,0 +1,9 @@
+import styles from './Suggestion.module.css';
+
+export const Suggestions = () => {
+    return (
+        <div>
+            <h1>Watch next</h1>
+        </div>
+    );
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,12 @@
 export { Navbar } from './Navbar/Navbar';
 export { PlaylistCard } from './PlaylistCard/PlaylistCard';
 export { VideoCard } from './VideoCard/VideoCard';
-export { PrivateRoute } from './PrivateRoute/PrivateRoute';
+export { Suggestions } from './Suggestions/Suggestions';
 export { LoginModal } from './Modal/LoginModal';
 export { Loader } from './Loader/Loader';
 export { alreadyExist } from './utility';
 export { InputField } from './InputFileds/InputField';
+export { PrivateRoute } from './PrivateRoute/PrivateRoute';
 export {
     successNotification,
     successRemoveNotification,
