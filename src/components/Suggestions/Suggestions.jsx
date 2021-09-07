ea0a2bb92473a7de468c1f2de15d38f17fbd4046
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-09-07 21:46:18 +0530

    Style: suggestion page

--- src/components/Suggestions/Suggestions.jsx
+++ src/components/Suggestions/Suggestions.jsx
@@ -1,9 +1,35 @@
+import { Link } from 'react-router-dom';
+import { useData } from '../../context';
 import styles from './Suggestion.module.css';
 
-export const Suggestions = () => {
+export const Suggestions = ({ currentVideoId }) => {
+    const { allVideos } = useData();
+
+    const suggestedVideos = allVideos
+        .filter((video) => video._id !== currentVideoId)
+        .splice(0, 5);
     return (
         <div>
             <h1>Watch next</h1>
+            <div>
+                {suggestedVideos &&
+                    suggestedVideos.map((video) => (
+                        <Link to={`/${video._id}`} className={`${styles.card}`}>
+                            <div>
+                                <img
+                                    src={video.image}
+                                    alt={video.title}
+                                    className={styles.cardImage}
+                                />
+                            </div>
+                            <div>
+                                <h1 className={styles.cardTitle}>
+                                    {video.title}
+                                </h1>
+                            </div>
+                        </Link>
+                    ))}
+            </div>
         </div>
     );
 };
