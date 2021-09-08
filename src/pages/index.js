0f11c4d8d4833fd3e6887f739aac7ec3246125ea
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-09-08 20:28:41 +0530

    Navbar updagte

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 export { default as Home } from './Home/Home';
 export { default as Login } from './Login/Login';
 export { default as SignUp } from './SignUp/SignUp';
-export { default as Account } from './Account/Account';
 export { default as Playlists } from './Playlist/Playlist';
 export { default as Playlistpage } from './Playlistpage/Playlistpage';
 export { default as VideoDetails } from './VideoDetails/VideoDetails';
